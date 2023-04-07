743765ed98a94faba664cf6bf21dc9601dd816a8
Hamilton <hamiltontruong@gmail.com>
2023-04-06 17:03:52 -0700

    Error handling for rate limit

--- app/api/coins_routes.py
+++ app/api/coins_routes.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, jsonify, session, current_app
+from flask import Blueprint, jsonify, session, current_app, make_response
 import requests
 from app.cache import cache
 
 coins_routes = Blueprint('coins', __name__)
+
+
+def handle_api_res(res):
+    if res.status_code == 200:
+        return jsonify(res.json())
+    elif res.status_code == 429:
+        return make_response(jsonify({"error": "Too many requests, please try again later."}), 429)
+    else:
+        return make_response(jsonify({"error": "An error occurred, please try again later."}), 500)
 
 
 @coins_routes.route('/featured')
@@ -14,8 +23,7 @@ def featured_cryptocurrencies():
     """
     res = requests.get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&locale=en")
-    data = res.json()
-    return jsonify(data)
+    return handle_api_res(res)
 
 
 @coins_routes.route('/explore')
@@ -23,8 +31,7 @@ def featured_cryptocurrencies():
 def explore_cryptocurrencies():
     res = requests.get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
-    data = res.json()
-    return jsonify(data)
+    return handle_api_res(res)
 
 
 @coins_routes.route('/<coin_id>')
@@ -32,5 +39,12 @@ def explore_cryptocurrencies():
 def details_cryptocurrency(coin_id):
     res = requests.get(
         f"https://api.coingecko.com/api/v3/coins/{coin_id}?localization=false")
-    data = res.json()
-    return jsonify(data)
+    return handle_api_res(res)
+
+
+@coins_routes.route('/<coin_id>/24h_chart')
+@cache.cached(timeout=60)
+def day_chart(coin_id):
+    res = requests.get(
+        f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency=usd&days=1")
+    return handle_api_res(res)
