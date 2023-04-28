8da3299ced39c2588a5172a34f2be971a51987d6
Hamilton <hamiltontruong@gmail.com>
2023-04-28 15:15:07 -0700

    update squiggly

--- react-app/src/components/HomePage/HeroGM/HeroGM.js
+++ react-app/src/components/HomePage/HeroGM/HeroGM.js
@@ -1,5 +1,6 @@
 import { useHistory } from "react-router-dom";
 import gmdeekay from "../../../assets/gmdeekay.mp4";
+import squiggly from "../../../assets/squiggly.png";
 
 const HeroGM = () => {
   const history = useHistory();
@@ -12,9 +13,16 @@ const HeroGM = () => {
       <section className="bg-[#2f3a58]">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-100">
-              GM
-            </h1>
+            <div className="flex">
+              <h1 className="max-w-2xl mr-4 mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-100">
+                GM
+              </h1>
+              <img
+                className="h-12 place-self-center"
+                src={squiggly}
+                alt="squiggly"
+              ></img>
+            </div>
             <p className="max-w-2xl mb-6 font-light text-gray-300 lg:mb-8 md:text-lg lg:text-xl">
               This is a personal project centered around exploring Tailwind CSS
               and working with cryptocurrency and NFT APIs. Enjoy!
