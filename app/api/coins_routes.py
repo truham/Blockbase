8f1cc2534498c00b0f988397e4207c8439c4b7f6
Hamilton <hamiltontruong@gmail.com>
2023-05-02 16:35:55 -0700

    update routes

--- app/api/coins_routes.py
+++ app/api/coins_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, session, current_app, make_response
 import requests
 from app.cache import cache
+import time
 
 coins_routes = Blueprint('coins', __name__)
 
@@ -14,59 +15,71 @@ def handle_api_res(res):
         return make_response(jsonify({"error": "An error occurred, please try again later."}), 500)
 
 
+# exponential backoff strategy, wait for increasing amnt of time between retries
+# reduces the chances of hitting the rate limit again
+def req_with_exponential_backoff(url, retries=3, backoff_factor=2):
+    for attempt in range(retries):
+        res = requests.get(url)
+        if res.status_code != 429:
+            return res
+        sleep_time = backoff_factor ** (attempt + 1)
+        time.sleep(sleep_time)
+    return res
+
+
 @coins_routes.route('/featured')
-@cache.cached(timeout=60)
+@cache.cached(timeout=300)
 def featured_cryptocurrencies():
     """
     Fetching data from Coingecko for 6 cryptocurrencies
     to be used in featured section on home page
     """
-    res = requests.get(
+    res = req_with_exponential_backoff(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&locale=en")
 
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError:
-        res = requests.get(
+        res = req_with_exponential_backoff(
             "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&locale=en")
 
     return handle_api_res(res)
 
 
 @coins_routes.route('/explore')
-@cache.cached(timeout=60)
+@cache.cached(timeout=300)
 def explore_cryptocurrencies():
-    res = requests.get(
+    res = req_with_exponential_backoff(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError:
-        res = requests.get(
+        res = req_with_exponential_backoff(
             "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
     return handle_api_res(res)
 
 
 @coins_routes.route('/<coin_id>')
-@cache.cached(timeout=60)
+@cache.cached(timeout=300)
 def details_cryptocurrency(coin_id):
-    res = requests.get(
+    res = req_with_exponential_backoff(
         f"https://api.coingecko.com/api/v3/coins/{coin_id}?localization=false")
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError:
-        res = requests.get(
+        res = req_with_exponential_backoff(
             f"https://api.coingecko.com/api/v3/coins/{coin_id}?localization=false")
     return handle_api_res(res)
 
 
 @coins_routes.route('/<coin_id>/24h_chart')
-@cache.cached(timeout=60)
+@cache.cached(timeout=300)
 def day_chart(coin_id):
-    res = requests.get(
+    res = req_with_exponential_backoff(
         f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency=usd&days=1")
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError:
-        res = requests.get(
+        res = req_with_exponential_backoff(
             f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart?vs_currency=usd&days=1")
     return handle_api_res(res)
