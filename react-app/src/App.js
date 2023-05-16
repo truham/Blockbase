29cfdfc190d08a581b79213101f561240bd9b428
Hamilton <hamiltontruong@gmail.com>
2023-05-16 14:51:09 -0700

    update connect button customization

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -13,6 +13,14 @@ import HeroMemes from "./components/HomePage/HeroMemes/HeroMemes";
 import Footer from "./components/Footer/Footer";
 import NFTView from "./components/NFTs/NFTView/NFTView";
 import NFTCollections from "./components/NFTs/NFTCollections/NFTCollections";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
 
 import "./index.css";
 
@@ -23,41 +31,67 @@ function App() {
     dispatch(authenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  const projectId = process.env.REACT_APP_PROJECT_ID;
+  const chains = [arbitrum, mainnet, polygon];
+
+  const { publicClient } = configureChains(chains, [
+    w3mProvider({ projectId }),
+  ]);
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, version: 2, chains }),
+    publicClient,
+  });
+
+  const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
   return (
     <>
-      <Navigation />
-      <div className="page-container">
-        <div className="content-wrap">
-          {isLoaded && (
-            <Switch>
-              <Route exact path="/">
-                <HeroGM />
-                <HeroMemes />
-                <Featured />
-              </Route>
-              <Route exact path="/nft/profile">
-                <NFTView />
-              </Route>
-              <Route exact path="/nft/collections/">
-                <NFTCollections />
-              </Route>
-              <Route exact path="/cryptocurrencies/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route>
-                <Explore exact path="/cryptocurrencies" />
-              </Route>
-              <Route path="/login">
-                <LoginFormPage />
-              </Route>
-              <Route path="/signup">
-                <SignupFormPage />
-              </Route>
-            </Switch>
-          )}
+      <WagmiConfig config={wagmiConfig}>
+        <Navigation />
+        <div className="page-container">
+          <div className="content-wrap">
+            {isLoaded && (
+              <Switch>
+                <Route exact path="/">
+                  <HeroGM />
+                  <HeroMemes />
+                  <Featured />
+                </Route>
+                <Route exact path="/nft/profile">
+                  <NFTView />
+                </Route>
+                <Route exact path="/nft/collections/">
+                  <NFTCollections />
+                </Route>
+                <Route exact path="/cryptocurrencies/:coinId">
+                  <CryptoDetails />
+                </Route>
+                <Route>
+                  <Explore exact path="/cryptocurrencies" />
+                </Route>
+                <Route path="/login">
+                  <LoginFormPage />
+                </Route>
+                <Route path="/signup">
+                  <SignupFormPage />
+                </Route>
+              </Switch>
+            )}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeVariables={{
+          "--w3m-background-color": "#344afb",
+          "--w3m-accent-color": "#344afb",
+        }}
+      />
     </>
   );
 }
--- react-app/src/components/Navigation/Navigation.js
+++ react-app/src/components/Navigation/Navigation.js
@@ -1,75 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import logo from "../../assets/blockbase-logo.png";
-import WalletConnect from "./WalletConnect";
+// import { Web3Button } from "@web3modal/react";
 
 function Navigation() {
   const history = useHistory();
   const [open, setOpen] = useState(false);
-  const [walletAddress, setWalletAddress] = useState("");
   const [showMenu, setShowMenu] = useState(false);
-
-  // useEffect(() => {
-  //   getCurrentWalletConnected();
-  //   addWalletListener();
-  // }, []);
-
-  // const connectWallet = async () => {
-  //   if (
-  //     typeof window !== "undefined" &&
-  //     typeof window.ethereum !== "undefined"
-  //   ) {
-  //     try {
-  //       // Metamask is installed
-  //       const accounts = await window.ethereum.request({
-  //         method: "eth_requestAccounts",
-  //       });
-  //       setWalletAddress(accounts[0]);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   } else {
-  //     // Metamask NOT installed
-  //     console.log("Please install MetaMask");
-  //   }
-  // };
-
-  // const getCurrentWalletConnected = async () => {
-  //   if (
-  //     typeof window !== "undefined" &&
-  //     typeof window.ethereum !== "undefined"
-  //   ) {
-  //     try {
-  //       const accounts = await window.ethereum.request({
-  //         method: "eth_accounts",
-  //       });
-  //       if (accounts.length > 0) {
-  //         console.log(accounts[0]);
-  //         setWalletAddress(accounts[0]);
-  //       } else {
-  //         console.log("Connect to MetaMask using the Connect button");
-  //       }
-  //     } catch (err) {
-  //       console.error(err.message);
-  //     }
-  //   } else {
-  //     console.log("Please install MetaMask");
-  //   }
-  // };
-
-  // const addWalletListener = async () => {
-  //   if (
-  //     typeof window !== "undefined" &&
-  //     typeof window.ethereum !== "undefined"
-  //   ) {
-  //     window.ethereum.on("accountsChanged", (accounts) => {
-  //       setWalletAddress(accounts[0]);
-  //       console.log(accounts[0]);
-  //     });
-  //   } else {
-  //     setWalletAddress("");
-  //   }
-  // };
 
   const logoHome = () => {
     history.push("/");
@@ -200,18 +137,9 @@ function Navigation() {
               </div>
             )}
           </li>
-          
-          <WalletConnect />
-          {/* <button
-            // onClick={connectWallet}
-            className="md:my-0 md:ml-8 bg-[#344afb] text-white px-4 py-2 rounded-lg hover:bg-[#2c3fd6]"
-          >
-            {walletAddress?.length > 0
-              ? `${walletAddress.substring(0, 6)}...${walletAddress.substring(
-                  38
-                )}`
-              : "Connect"}
-          </button> */}
+
+          <w3m-core-button icon="hide" label="Connect"></w3m-core-button>
+          {/* <Web3Button /> */}
         </ul>
       </div>
     </div>
