70aa5136f1f31b02b1a0be5901476e935211145c
Hamilton <hamiltontruong@gmail.com>
2023-05-28 11:01:53 -0700

    fix footer, start chatbot

--- react-app/src/components/Chat/index.js
+++ react-app/src/components/Chat/index.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Chat = () => {
   return (
-    <>
-      <h1>Chat</h1>
-    </>
+    <div className="flex row h-screen bg-[#343541] text-white">
+      <section className="w-60 bg-[#202124]">
+        <button className="">+ New Chat</button>
+        <ul className="chat-history"></ul>
+        <nav>
+          <p>Chat Bot</p>
+        </nav>
+      </section>
+      <section className="w-100">Chat Section</section>
+    </div>
   );
 };
 
 export default Chat;
- 
