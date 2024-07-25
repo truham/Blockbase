3197aba314853c8e7c1925c309c7cbbe65ae22ca
truham <hamiltontruong@gmail.com>
2024-07-24 22:08:21 -0700

    Test deploy

--- frontend/src/services/coinService.ts
+++ frontend/src/services/coinService.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 import { Coin } from "../types";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+console.log("API URL:", apiUrl);
 
 export const getCoins = async (): Promise<Coin[]> => {
+  console.log("Fetching coins from:", `${apiUrl}/api/coins`);
   const response = await axios.get(`${apiUrl}/api/coins`);
   return response.data;
 };
