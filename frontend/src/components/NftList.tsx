4f4d397575fbd4f3515c5e8fa0e20710128ab3bd
truham <hamiltontruong@gmail.com>
2024-07-27 01:02:13 -0700

    Comment out NftList for now

--- frontend/src/components/NftList.tsx
+++ frontend/src/components/NftList.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import { fetchNFTs } from "../store/nftSlice";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
+// import React, { useEffect } from "react";
+// import { fetchNFTs } from "../store/nftSlice";
+// import { useAppDispatch, useAppSelector } from "../store/hooks";
 
-interface NftListProps {
-  address: string;
-}
+// interface NftListProps {
+//   address: string;
+// }
 
-const NftList: React.FC<NftListProps> = ({ address }) => {
-  const dispatch = useAppDispatch();
-  const { nfts, loading, error } = useAppSelector((state) => state.nfts);
+// const NftList: React.FC<NftListProps> = ({ address }) => {
+//   const dispatch = useAppDispatch();
+//   const { nfts, loading, error } = useAppSelector((state) => state.nfts);
 
-  useEffect(() => {
-    dispatch(fetchNFTs(address));
-  }, [dispatch, address]);
+//   useEffect(() => {
+//     dispatch(fetchNFTs(address));
+//   }, [dispatch, address]);
 
-  if (loading) return <div>Loading NFTs...</div>;
-  if (error) return <div>Error: {error}</div>;
+//   if (loading) return <div>Loading NFTs...</div>;
+//   if (error) return <div>Error: {error}</div>;
 
-  return (
-    <div className="nft-list">
-      <h2 className="text-2xl font-bold">NFTs for {address}</h2>
-      <ul>
-        {nfts.map((nft) => (
-          <li key={nft.id}>{nft.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div className="nft-list">
+//       <h2 className="text-2xl font-bold">NFTs for {address}</h2>
+//       <ul>
+//         {nfts.map((nft) => (
+//           <li key={nft.id}>{nft.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default NftList;
+// export default NftList;
