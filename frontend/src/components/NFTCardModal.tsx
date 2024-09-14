93a93e509185999500b23476f8e95b62ebae6989
truham <hamiltontruong@gmail.com>
2024-09-13 20:08:56 -0700

    Increase modal size

--- frontend/src/components/NFTCardModal.tsx
+++ frontend/src/components/NFTCardModal.tsx
@@ -40,7 +40,7 @@ const NFTCardModal: React.FC<NFTCardModalProps> = ({
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div
         ref={modalRef}
-        className="bg-white p-6 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto"
+        className="bg-white p-6 rounded-lg w-11/12 h-5/6 max-w-6xl overflow-y-auto"
       >
         <button onClick={onClose} className="float-right text-2xl font-bold">
           &times;
