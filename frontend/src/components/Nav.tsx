2915164ee50c6dc304926c155b9fd4a0ef79e6fc
truham <hamiltontruong@gmail.com>
2024-09-22 17:45:15 -0700

    Update route and name for portfolio

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -67,7 +67,7 @@ const Nav: React.FC = () => {
               >
                 <ul className="py-2 text-sm text-gray-700">
                   <li>
-                    <Link href="/nfts/portfolio-appraisal" legacyBehavior>
+                    <Link href="/nfts/portfolio" legacyBehavior>
                       <a className="block px-4 py-2 hover:bg-gray-100">
                         Portfolio Appraisal
                       </a>
